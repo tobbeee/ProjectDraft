271a04bb10983f359abf2b53657029a64e44d754
Tobbe <tobbe_weentoz@hotmail.com>
2022-11-17 15:48:34 +0100

    trying

--- verse2020/training/main_spine_localization.py
+++ verse2020/training/main_spine_localization.py
@@ -210,7 +210,7 @@ class MainLoop(MainLoopBase):
         """
         print("Spine loc training...")
         image, target_landmarks, image_id = self.dataset_train_iter.get_next()
-        print(image.numpy())
+        print(tf.Tensor.shape(image))
         with tf.GradientTape() as tape:
             _, losses = self.call_model_and_loss(image, target_landmarks, training=True)
             if self.reg_constant > 0:
